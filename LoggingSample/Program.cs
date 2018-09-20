8cfd64e511d0431b6d8ec8dd9be33e3d7c113512
t03apt <t03apt@hotmail.com>
2018-09-21 00:34:24 +0200

    add: Logging sample

--- LoggingSample/Program.cs
+++ LoggingSample/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Runtime.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Formatting.Compact;
 
 namespace LoggingSample
 {
@@ -6,7 +12,82 @@ namespace LoggingSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteHeader("Microsoft.Extensions.Logging.ConsoleLoggerExtensions.AddConsole()");
+
+            RunTest(builder => builder.AddConsole());
+
+            WriteHeader("Serilog.WriteTo.Console()");
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithExceptionDetails()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            RunTest(builder => builder.AddSerilog());
+
+            WriteHeader("Serilog.WriteTo.Console(new CompactJsonFormatter())");
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithExceptionDetails()
+
+                .WriteTo.Console(new CompactJsonFormatter())
+              .CreateLogger();
+
+            RunTest(builder => builder.AddSerilog());
         }
+
+        private static void WriteHeader(string header)
+        {
+            Console.WriteLine("\r\n---- {0} ----\r\n", header);
+        }
+
+        private static void RunTest(Action<ILoggingBuilder> configureLogging)
+        {
+            var services = new ServiceCollection();
+            services.AddLogging(builder => configureLogging(builder));
+
+            using (var serviceProvider = services.BuildServiceProvider(true))
+            {
+                var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+                try
+                {
+                    ThrowCustomException();
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Error. {SampleProperty}", "Sample property value");
+                }
+            }
+        }
+
+        private static void ThrowCustomException()
+        {
+            var ex = new CustomException("Custom exception message");
+            ex.Data["myProperty"] = "my property value";
+            throw ex;
+        }
+    }
+
+    public class CustomException : Exception
+    {
+        public CustomException()
+        {
+        }
+
+        public CustomException(string message) : base(message)
+        {
+        }
+
+        public CustomException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CustomException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public string MyProperty { get; set; } = "My custom property";
+
     }
 }
